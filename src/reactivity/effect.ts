9e8c7f43b4507f31196e25fa7cdb60737b6bcf89
fjl <18618166564@163.com>
2022-06-11 17:14:52 +0800

    scheduler

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,11 +1,11 @@
 class ReactiveEffect {
   private _fn: any;
-  constructor(fn: any) {
+  constructor(fn: any, public scheduler?) {
     this._fn = fn;
   }
   run() {
     activeEffect = this;
-    this._fn();
+    return this._fn();
   }
 }
 const targetMap = new Map();
@@ -27,10 +27,15 @@ export function trigger(target, key) {
   let depsMap = targetMap.get(target);
   let dep = depsMap.get(key);
   for (const effect of dep) {
-    effect.run();
+    if (effect.scheduler) {
+      effect.scheduler();
+    } else {
+      effect.run();
+    }
   }
 }
-export function effect(fn: any) {
-  const _effect = new ReactiveEffect(fn);
+export function effect(fn: any, options:any={}) {
+  const _effect = new ReactiveEffect(fn,options.scheduler);
   _effect.run();
+  return _effect.run.bind(_effect);
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -8,10 +8,44 @@ describe("effect", () => {
     });
     let nextAge;
     effect(() => {
-      nextAge = user.age +1;
+      nextAge = user.age + 1;
     });
     expect(nextAge).toBe(11);
     user.age++;
     expect(nextAge).toBe(12);
+  });
+  it("should return runner when call effect", () => {
+    let foo = 10;
+    const runner = effect(() => {
+      foo++;
+      return "foo";
+    });
+    expect(foo).toBe(11);
+    const r = runner();
+    expect(foo).toBe(12);
+    expect(r).toBe("foo");
+  });
+  it("scheduler", () => {
+    let dummy;
+    let run;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    });
+    const obj = reactive({ foo: 1 });
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      {
+        scheduler,
+      }
+    );
+    expect(scheduler).not.toHaveBeenCalled();
+    expect(dummy).toBe(1);
+    obj.foo++;
+    expect(scheduler).toHaveBeenCalledTimes(1);
+    expect(dummy).toBe(1);
+    run();
+    expect(dummy).toBe(2);
   });
 });
