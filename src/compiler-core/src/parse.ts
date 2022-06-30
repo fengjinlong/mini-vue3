949f740aa67589166c65dc96d7f6940badb0e94d
fjl <18618166564@163.com>
2022-06-30 09:08:03 +0800

    interpolation

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,22 +1,43 @@
+import { NodeTypes } from "./ast";
+
 export function baseParse(content: string) {
   const context = createParserContext(content);
   return createRoot(parseChildren(context));
 }
 function parseChildren(context) {
   const nodes: any = [];
-  const node = parseInterpolation();
+  let node;
+  if (context.source.startsWith("{{")) {
+    node = parseInterpolation(context);
+  }
   nodes.push(node);
   return nodes;
 }
-function parseInterpolation() {
+function parseInterpolation(context) {
+  const openDelimiter = "{{";
+  const closeDelimiter = "}}";
+
+  const closeIndex = context.source.indexOf(
+    closeDelimiter,
+    openDelimiter.length
+  );
+  advanceBy(context, openDelimiter.length);
+  const rawContentLength = closeIndex - openDelimiter.length;
+  const rawContent = context.source.slice(0, rawContentLength);
+  const content = rawContent.trim();
+  advanceBy(context, rawContentLength + closeDelimiter.length);
   return {
-    type: "interpolation",
+    type: NodeTypes.INTERPOLATION,
     content: {
-      type: "simple_expression",
-      content: "message",
+      type: NodeTypes.SIMPLE_EXPRESSION,
+      content: content,
     },
   };
 }
+function advanceBy(context: any, length) {
+  context.source = context.source.slice(length);
+}
+
 function createRoot(children) {
   return {
     children,
