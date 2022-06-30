df576960489279b392a3dacab89234a616c7eed8
fjl <18618166564@163.com>
2022-06-30 17:24:39 +0800

    parse

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -5,28 +5,49 @@ const enum TagType {
 }
 export function baseParse(content: string) {
   const context = createParserContext(content);
-  return createRoot(parseChildren(context));
+  return createRoot(parseChildren(context, []));
 }
-function parseChildren(context) {
+function parseChildren(context, ancestors) {
   const nodes: any = [];
-  let node;
-  const s = context.source;
-  if (s.startsWith("{{")) {
-    node = parseInterpolation(context);
-  } else if (s[0] === "<") {
-    if (/[a-z]/i.test(s[1])) {
-      node = parseElement(context);
+  while (!isEnd(context, ancestors)) {
+    let node;
+    const s = context.source;
+    if (s.startsWith("{{")) {
+      node = parseInterpolation(context);
+    } else if (s[0] === "<") {
+      if (/[a-z]/i.test(s[1])) {
+        node = parseElement(context, ancestors);
+      }
     }
+    if (!node) {
+      node = parseText(context);
+    }
+    nodes.push(node);
   }
-  if (!node) {
-    node = parseText(context);
-  }
-  nodes.push(node);
+
   return nodes;
 }
+function isEnd(context, ancestors) {
+  const s = context.source;
+
+  for (let i = ancestors.length - 1; i >= 0; i--) {
+    const tag = ancestors[i].tag;
+    if (startWithEndTagOpen(s, tag)) {
+      return true;
+    }
+  }
+  return !s;
+}
 function parseText(context: any) {
-  const content: any = parseTextData(context, context.source.length);
-  advanceBy(context, content.length);
+  let endIndex = context.source.length;
+  let endTokens = ["{{", "<"];
+  for (let i = 0; i < endTokens.length; i++) {
+    const index = context.source.indexOf(endTokens[i]);
+    if (index !== -1 && endIndex > index) {
+      endIndex = index;
+    }
+  }
+  const content: any = parseTextData(context, endIndex);
   return {
     type: NodeTypes.TEXT,
     content,
@@ -38,14 +59,27 @@ function parseTextData(context, length) {
   advanceBy(context, length);
   return content;
 }
-function parseElement(context: any): any {
-  const element = parseTag(context, TagType.Start);
-  parseTag(context, TagType.End);
+function parseElement(context: any, ancestors): any {
+  const element: any = parseTag(context, TagType.Start);
+  ancestors.push(element);
+  element.children = parseChildren(context, ancestors);
+  ancestors.pop();
+  if (startWithEndTagOpen(context.source, element.tag)) {
+    parseTag(context, TagType.End);
+  } else {
+    throw new Error(`缺少 ${element.tag}`);
+  }
   return element;
 }
+function startWithEndTagOpen(source: any, tag: any) {
+  return (
+    source.startsWith("</") &&
+    source.slice(2, 2 + tag.length).toLowerCase() === tag.toLowerCase()
+  );
+}
+
 function parseTag(context: any, type: TagType) {
   const match: any = /^<\/?([a-z]*)/i.exec(context.source);
-  console.log(match);
   const tag = match[1];
   advanceBy(context, match[0].length);
   advanceBy(context, 1);
@@ -67,7 +101,6 @@ function parseInterpolation(context) {
   advanceBy(context, openDelimiter.length);
   const rawContentLength = closeIndex - openDelimiter.length;
   const rawContent = parseTextData(context, rawContentLength);
-  //  context.source.slice(0, rawContentLength);
   const content = rawContent.trim();
   advanceBy(context, closeDelimiter.length);
   return {
